558fce81d577fd3c4db1b803f0ca04e19ec2c238
abcdefgs0324 <abcdefgs0324@gmail.com>
2017-02-23 23:13:41 +0800

    Finished hw1 program.

--- hw1/hw1_102703039.R
+++ hw1/hw1_102703039.R
@@ -9,6 +9,7 @@ output_file <- ''
 if (length(args) < 4) {
     stop("USAGE: Rscript hw1_exam.R -files [input-file] -out [result-file]", call.=FALSE)
 } else {
+    # get input/output file
     for (i in c(1:length(args))) {
         if (args[i] == '-files') {
             input_file = args[i+1]
@@ -20,13 +21,18 @@ if (length(args) < 4) {
     if (input_file == '' || output_file == '') {
         stop("USAGE: Rscript hw1_exam.R -files input -out result", call.=FALSE)
     }
+    if (!file.exists(input_file)) {
+        stop("ERROR: No such input file, please check it.")
+    }
 }
 
-data <- read.table(input_file, header=TRUE, sep=',')
+# read file
+data <- read.csv(input_file, header=TRUE, sep=',')
 
+# define the function getting the max value
 getMax <- function (data) {
     max <- data[1]
-    for (i in 2:length(data)) {
+    for (i in c(2:length(data))) {
         if (data[i] > max) {
             max <- data[i]
         }
@@ -34,8 +40,16 @@ getMax <- function (data) {
     return(round(max, 2))
 }
 
-maxWeight <- getMax(data[,2])
-maxHeight <- getMax(data[,3])
+setName <- sub('.csv$', '', input_file)
+maxWeight <- getMax(data[,'weight'])
+maxHeight <- getMax(data[,'height'])
 
-print(maxWeight)
-print(maxHeight)
+set <- c(setName)
+weight <- c(maxWeight)
+height <- c(maxHeight)
+
+data <- data.frame(set, weight, height)
+
+# write file
+write.csv(data, file=output_file, row.names=FALSE, quote=FALSE)
+
