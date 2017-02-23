7d684d710135ebf5c74e66c9fc2de95bf8771bfa
abcdefgs0324 <abcdefgs0324@gmail.com>
2017-02-23 18:00:53 +0800

    Added sample input/output files.

--- hw1/hw1_102703039.R
+++ hw1/hw1_102703039.R
@@ -3,9 +3,39 @@
 ########################
 
 args = commandArgs(trailingOnly=TRUE)
-if (length(args)==0) {
-  stop("USAGE: Rscript hw1_exam.R input", call.=FALSE)
-} else if (length(args)==1) {
-  i_f <- args[1] 
+input_file <- ''
+output_file <- ''
+
+if (length(args) < 4) {
+    stop("USAGE: Rscript hw1_exam.R -files [input-file] -out [result-file]", call.=FALSE)
+} else {
+    for (i in c(1:length(args))) {
+        if (args[i] == '-files') {
+            input_file = args[i+1]
+        }
+        else if (args[i] == '-out') {
+            output_file = args[i+1]
+        }
+    }
+    if (input_file == '' || output_file == '') {
+        stop("USAGE: Rscript hw1_exam.R -files input -out result", call.=FALSE)
+    }
 }
-print(i_f)
+
+data <- read.table(input_file, header=TRUE, sep=',')
+
+getMax <- function (data) {
+    max <- data[1]
+    for (i in 2:length(data)) {
+        if (data[i] > max) {
+            max <- data[i]
+        }
+    }
+    return(round(max, 2))
+}
+
+maxWeight <- getMax(data[,2])
+maxHeight <- getMax(data[,3])
+
+print(maxWeight)
+print(maxHeight)
