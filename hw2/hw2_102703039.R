9a3e973b9b5a8b08370b7a225d991cb0a1ae8b1e
abcdefgs0324 <abcdefgs0324@gmail.com>
2017-03-11 01:46:31 +0800

    Addressed several input files with list.

--- hw2/hw2_102703039.R
+++ hw2/hw2_102703039.R
@@ -1,12 +1,12 @@
 ########################
 # homework2
-# GOAL: Get the max value and output new table.
+# GOAL: Get the max value from several tables and output new table.
 # DATE: 17/03/11
 ########################
 
 args = commandArgs(trailingOnly=TRUE)
-input_file <- ''
-output_file <- ''
+input_files <- c()
+output_file <- c()
 
 if (length(args) < 4) {
     stop("USAGE: Rscript hw2_102703039.R -files [input-files] -out [result-file]", call.=FALSE)
@@ -14,22 +14,25 @@ if (length(args) < 4) {
     # get input/output file
     for (i in c(1:length(args))) {
         if (args[i] == '-files') {
-            input_file = args[i+1]
+            j <- i+1
+            while (j < length(args)+1 && args[j] != '-out') {
+                input_files <- append(input_files, args[j])
+                j <- j + 1
+            }
         }
         else if (args[i] == '-out') {
-            output_file = args[i+1]
+            output_file <- args[i+1]
         }
     }
-    if (input_file == '' || output_file == '') {
+    if (is.null(input_files) || is.null(output_file)) {
         stop("USAGE: Rscript hw2_102703039.R -files [input-files] -out [result-file]", call.=FALSE)
     }
-    if (!file.exists(input_file)) {
-        stop("ERROR: No such input file, please check it.")
+    for (input in input_files) {
+        if (!file.exists(input)) {
+            stop("ERROR: No such input file, please check it.")
+        }
     }
 }
-
-# read file
-data <- read.csv(input_file, header=TRUE, sep=',')
 
 # define the function getting the max value
 getMax <- function (data) {
@@ -42,9 +45,36 @@ getMax <- function (data) {
     return(round(max, 2))
 }
 
-setName <- sub('.csv$', '', input_file)
-maxWeight <- getMax(data[,'weight'])
-maxHeight <- getMax(data[,'height'])
+# define the function getting the index of the max value
+getMaxIndex <- function (data) {
+    index <- 1
+    for (i in c(2:length(data))) {
+        if (data[i] > data[index]) {
+            index <- i
+        }
+    }
+    return(index)
+}
+
+# initialize result list
+setName <- c()
+maxWeight <- c()
+maxHeight <- c()
+
+for (input in input_files) {
+    # read file
+    data <- read.csv(input, header=TRUE, sep=',')
+
+    # accumulate max values
+    setName <- append(setName, sub('.csv$', '', input))
+    maxWeight <- append(maxWeight, getMax(data[,'weight']))
+    maxHeight <- append(maxHeight, getMax(data[,'height']))
+}
+
+# conclude the result
+setName <- append(setName, 'max')
+maxWeight <- append(maxWeight, sub('.csv$', '', input_files[getMaxIndex(maxWeight)]))
+maxHeight <- append(maxHeight, sub('.csv$', '', input_files[getMaxIndex(maxHeight)]))
 
 set <- c(setName)
 weight <- c(maxWeight)
