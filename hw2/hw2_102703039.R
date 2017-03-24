20fee65eaa00415528f034b1d26c211e39a69b27
abcdefgs0324 <abcdefgs0324@gmail.com>
2017-03-25 01:39:43 +0800

    Finished hw2 program.

--- hw2/hw2_102703039.R
+++ hw2/hw2_102703039.R
@@ -1,67 +1,118 @@
 ########################
 # homework2
 # GOAL: Accumulate sensitivity, specificity, F1 score and AUC for several input files.
-# DATE: 17/03/24
+# DATE: 17/03/25
 ########################
 
-query_func<-function(query_m, i)
+query_func <- function(query_m, i)
 {
-  if(query_m == "male"){
-    which.min(i)
-  }
-  else if (query_m == "female") {
-    which.max(i)
-  } else {
-    stop(paste("ERROR: unknown query function", query_m))
-  }
+    if(query_m == "male"){
+        which.max(i)
+    }
+    else if (query_m == "female") {
+        which.max(i)
+    } else {
+        stop(paste("ERROR: unknown query function", query_m))
+    }
+}
+
+getConfusionMatrix <- function(pres, refs) {
+    return(table(prediction=pres, reference=refs))
+}
+
+getSensitivity <- function(conf, target, oppo) {
+    return(conf[target, target] / sum(conf[, target]))
+}
+
+getSpecificity <- function(conf, target, oppo) {
+    return(conf[oppo, oppo] / sum(conf[, oppo]))
+}
+
+getPrecision <- function(conf, target, oppo) {
+    return(conf[target, target] / sum(conf[target,]))
+}
+
+getF1Score <- function(conf, target, oppo) {
+    prec <- getPrecision(conf, target, oppo)
+    reca <- getSensitivity(conf, target, oppo)
+    return(2 * prec * reca / (prec + reca))
+}
+
+getAUC <- function(scores, refs) {
+    return(attributes(performance(prediction(scores, refs), "auc"))$y.values[[1]])
 }
 
 # read parameters
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
-  stop("USAGE: Rscript hw2_102703039.R --target male/female --files file1 file2 ... filen –out out.csv", call.=FALSE)
+    stop("USAGE: Rscript hw2_102703039.R --target male/female --files file1 file2 ... filen –-out out.csv", call.=FALSE)
 }
 
+library("ROCR")
 # parse parameters
-i<-1
-while(i < length(args))
-{
-  if(args[i] == "-query"){
-    query_m<-args[i+1]
-    i<-i+1
-  }else if(args[i] == "-files"){
-    j<-grep("-", c(args[(i+1):length(args)], "-"))[1]
-    files<-args[(i+1):(i+j-1)]
-    i<-i+j-1
-  }else if(args[i] == "-out"){
-    out_f<-args[i+1]
-    i<-i+1
-  }else{
-    stop(paste("Unknown flag", args[i]), call.=FALSE)
-  }
-  i<-i+1
+i <- 1
+while(i < length(args)) {
+    if(args[i] == "--target"){
+        query_m <- args[i+1]
+        if (query_m == "male")
+            oppo <- "female"
+        else
+            oppo <- "male"
+        i <- i+1
+    }else if(args[i] == "--files"){
+        j <- grep("-", c(args[(i+1):length(args)], "-"))[1]
+        files <- args[(i+1):(i+j-1)]
+        i <- i+j-1
+    }else if(args[i] == "--out"){
+        out_f <- args[i+1]
+        i <- i+1
+    }else{
+        stop(paste("Unknown flag", args[i]), call.=FALSE)
+    }
+    i <- i+1
 }
 
+# print some logs
 print("PROCESS")
 print(paste("query mode :", query_m))
 print(paste("output file:", out_f))
 print(paste("files      :", files))
 
-# read files
-names<-c()
-weis<-c()
-heis<-c()
+# init list
+names <- c()
+sens <- c()
+spes <- c()
+f1s <- c()
+aucs <- c()
+
 for(file in files)
 {
-  name<-gsub(".csv", "", basename(file))
-  d<-read.table(file, header=T,sep=",")
-  weis<-c(weis, d$weight[query_func(query_m, d$weight)])
-  heis<-c(heis, d$height[query_func(query_m, d$height)])
-  names<-c(names,name)
+    # read file
+    name <- gsub(".csv", "", basename(file))
+    d <- read.table(file, header=T, sep=",")
+    names <- c(names, name)
+
+    # calculate all data
+    confMatrix <- getConfusionMatrix(d$prediction, d$reference)
+    sen <- round(getSensitivity(confMatrix, query_m, oppo), 2)
+    spe <- round(getSpecificity(confMatrix, query_m, oppo), 2)
+    f1 <- round(getF1Score(confMatrix, query_m, oppo), 2)
+    if (query_m == "male")
+        auc <- round(getAUC(d$pred.score, d$reference), 2)
+    else
+        auc <- round(getAUC(1 - d$pred.score, d$reference), 2)
+
+    # append result
+    sens <- c(sens, sen)
+    spes <- c(spes, spe)
+    f1s <- c(f1s, f1)
+    aucs <- c(aucs, auc)
 }
-out_data<-data.frame(set=names, wei=weis, hei=heis, stringsAsFactors = F)
-index<-sapply(out_data[,c("wei","hei")], query_func, query_m=query_m)
+
+# conclude the result
+out_data <- data.frame(method=names, sensitivity=sens, specificity=spes, F1=f1s, AUC=aucs, stringsAsFactors = F)
+index <- sapply(out_data[, c("sensitivity", "specificity", "F1", "AUC")], query_func, query_m=query_m)
+out_data<-rbind(out_data, c("highest", names[index]))
 
 # output file
-out_data<-rbind(out_data,c(query_m,names[index]))
-write.table(out_data, file=out_f, row.names = F, quote = F)
+write.table(out_data, file=out_f, row.names=F, quote=F, sep=",")
